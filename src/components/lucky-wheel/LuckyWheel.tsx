a319f395eda9bd342892dc6f115cef7bf505713b
dapp-sculptor <hugodevpro@outlook.com>
2024-02-19 20:57:15 +0900

    feat: wallet

---
+++ src/components/lucky-wheel/LuckyWheel.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useRef } from 'react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { Props } from '.';
+
+export const LuckyWheel = ({ id, styleRotate, prizes, spinning, timeNeedleRotate }: Props) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const arrowRef = useRef<HTMLDivElement | null>(null);
+
+  /**
+   * function to drawl lucky wheel with canvas
+   * @param prizes is list prize
+   */
+  const drawWheel = (prizes: { name: string; img: string; percentpage: number; }[]) => {
+    const num = prizes.length;
+    const rotateDeg = 360 / num / 2 + 90;
+    const html = [];
+
+    const ele = document.getElementById(id);
+    const ulElementFirstRender = document.querySelector('.luckywheel-list');
+
+    if (ulElementFirstRender) {
+      ulElementFirstRender.remove();
+    }
+
+    if (ele) {
+      const prizeItems = document.createElement('ul');
+      const container = ele.querySelector('.luckywheel-container');
+
+      if (canvasRef.current && container) {
+        const ctx = canvasRef.current.getContext('2d')!;
+        for (let i = 0; i < num; i++) {
+          ctx.save();
+          ctx.beginPath();
+          ctx.translate(250, 250); // Center Point
+          ctx.moveTo(0, 0);
+          ctx.rotate((((360 / num) * i - rotateDeg) * Math.PI) / 180);
+          ctx.arc(0, 0, 250, 0, (2 * Math.PI) / num, false); // Radius
+          ctx.fillStyle = '#ffffff';
+          ctx.fill();
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = '#1A2B57';
+          ctx.stroke();
+          ctx.restore();
+
+          const htmlString = ``;
+
+          html.push(htmlString);
+        }
+        prizeItems.className = 'luckywheel-list';
+        container.appendChild(prizeItems);
+        prizeItems.innerHTML = html.join('');
+      }
+    }
+  };
+
+  function rotateArrow(spinningLuckyWheel?: boolean, timeNeedleRotate?: number) {
+    if (arrowRef.current) {
+      if (spinningLuckyWheel && timeNeedleRotate) {
+        arrowRef.current.style.animation = `rotate ${timeNeedleRotate}s linear infinite`;
+      } else {
+        arrowRef.current.style.animation = '';
+      }
+    }
+    requestAnimationFrame(() => rotateArrow(spinningLuckyWheel, timeNeedleRotate));
+  }
+
+  useEffect(() => {
+    void rotateArrow(spinning, timeNeedleRotate);
+  }, [spinning, arrowRef, timeNeedleRotate]);
+
+  useEffect(() => {
+    void drawWheel(prizes);
+  }, [prizes]);
+
+  return (
+    <div className='wrapper sm:w-[300px] md:w-[600px]' id='wrapper'>
+      <section id='luckywheel' className='luckywheel'>
+        <div className='luckywheel-btn'>
+          <div ref={arrowRef} className='luckywheel-btn-icon '>
+            <FaMapMarkerAlt className='text-[60px] text-[#1A2B57]' />
+          </div>
+        </div>
+
+        <div
+          className='luckywheel-container'
+          style={styleRotate.deg !== 0
+            ? {
+              transform: `rotate(${styleRotate.deg}deg)`,
+              transitionTimingFunction: styleRotate.timingFunc,
+              transitionDuration: `${styleRotate.timeDuration}s`
+            }
+            : {}}
+        >
+          <canvas ref={canvasRef} className='luckywheel-canvas' width={'500px'} height={'500px'} />
+        </div>
+
+        <div className='luckywheel-logo flex border-2 border-[#1A2B57]'>
+          <img src={'vite.svg'} className='p-2' />
+        </div>
+      </section>
+    </div>
+  );
+};
