3ca9fb943e085f25bb024ab63bf92f7a3d7ff9c8
dapp-sculptor <hugodevpro@outlook.com>
2024-03-12 10:42:53 -0700

    fix: final ts error

--- src/components/wallet-provider/index.tsx
+++ src/components/wallet-provider/index.tsx
@@ -4,10 +4,9 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import * as web3 from "@solana/web3.js";
 import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 import "@solana/wallet-adapter-react-ui/styles.css";
-import { net, rpcURL } from "@/data/constant";
+import { rpcURL } from "@/data/constant";
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
